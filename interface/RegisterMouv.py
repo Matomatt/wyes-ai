778916ba5b516bd23703c5109c45ddb9b5f1b4bb
HugJax <31313497+HugJax@users.noreply.github.com>
2021-02-28 14:41:57 +0100

    Update RegisterMouv.py

--- interface/RegisterMouv.py
+++ interface/RegisterMouv.py
@@ -30,11 +30,14 @@ class Regis(tk.Frame):
 
             self.bagain = tk.Button(self, text="Recommencer ou supprimer le mouvement", command=self.recommencer)
             self.bagain.configure(fg='lightgray', bg='#b30000')
-            self.bagain.pack(side=tk.BOTTOM, pady=50)
+            self.bagain.pack(side=tk.BOTTOM, pady=120)
 
             if self.statut==True:
                 self.bregis = tk.Button(self, text="Nouvel essai : " + str(self.essai + 1) + "/10", command=self.progression)
-            self.bregis.pack(side=tk.TOP, pady=40)
+                self.btrain = tk.Button(self, text="Training", command=self.training)
+                self.btrain.configure(fg='lightgray', bg='#2d8659')
+            self.bregis.pack(side=tk.TOP, pady=20)
+            self.btrain.pack(side=tk.TOP, pady=20)
 
 
 
@@ -65,6 +68,37 @@ class Regis(tk.Frame):
         # | | | | | | | | | | | | | | |
         # V V V V V V V V V V V V V V V
 
+        # A A A A A A A A A A A A A A A
+        # | | | | | | | | | | | | | | |
+        # | | | | CODES A MODIF | | | |
+        # | | | | | | | | | | | | | | |
+        self.newmouv()
+        
+    def training(self):
+    if self.statut == True:
+        self.statut = False
+        self.progress = ttk.Progressbar(self, orient="horizontal",
+                                        length=240, mode="determinate")
+        self.progress.pack()
+        self.progress.start()
+
+        for i in range(12):
+            self.progress["value"] = i * 9
+            self.update()
+            time.sleep(0.1)
+            if i == 11:
+                self.statut = True
+        self.progress.stop()
+
+        # Appel pour lancer un essai
+        # | | | | | | | | | | | | | | |
+        # | | | | CODES A MODIF | | | |
+        # | | | | | | | | | | | | | | |
+        # V V V V V V V V V V V V V V V
+        
+        gv.recordedMovements[self.nummouv-1].append(le.start())
+        print(gv.recordedMovements[self.nummouv-1])
+        
         # A A A A A A A A A A A A A A A
         # | | | | | | | | | | | | | | |
         # | | | | CODES A MODIF | | | |
