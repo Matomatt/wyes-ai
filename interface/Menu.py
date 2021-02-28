6ef094ea50a50cad9295f73a7d0c86215ec53c4b
HugJax <31313497+HugJax@users.noreply.github.com>
2021-02-28 15:11:19 +0100

    Ajout du bouton trainn

--- interface/Menu.py
+++ interface/Menu.py
@@ -1,6 +1,7 @@
 import tkinter as tk
 from Controle import Controle
 from Mouvements import Mouvements
+from MouvTraining import MouvTraining
 import global_variables as gv
 
 
@@ -17,15 +18,18 @@ class Menu(tk.Frame):
         self.t1.pack(pady=20)
 
         if(Mouvements.nbMouv(self)==3):
-            self.b1 = tk.Button(self, text="Prendre le contrôle", width=55, height=50, command=self.cont)
+            self.b1 = tk.Button(self, text="Prendre le contrôle", width=33, height=45, command=self.cont)
             self.b1.configure(fg='white', bg='#008080', activebackground='#009999', overrelief=tk.FLAT, relief=tk.FLAT)
         else:
-            self.b1 = tk.Button(self, text="Prendre les contrôle", width=55, height=50)
+            self.b1 = tk.Button(self, text="Prendre les contrôle", width=33, height=45)
             self.b1.configure(fg='white', bg='#969696', overrelief=tk.FLAT, relief=tk.FLAT, cursor='X_cursor')
-        self.b2 = tk.Button(self, text="Mouvements définis", width=55, height=50, command=self.mouv)
+        self.b2 = tk.Button(self, text="Mouvements définis", width=33, height=45, command=self.mouv)
         self.b2.configure(fg='white', bg='#008080', activebackground='#009999', overrelief=tk.FLAT, relief=tk.FLAT)
+        self.b3 = tk.Button(self, text="Training", width=33, height=45, command=self.train)
+        self.b3.configure(fg='white', bg='#008080', activebackground='#009999', overrelief=tk.FLAT, relief=tk.FLAT)
         self.b1.pack(in_=self, side=tk.LEFT, padx=10, pady=10)
-        self.b2.pack(in_=self, side=tk.RIGHT, padx=10, pady=10)
+        self.b2.pack(in_=self, side=tk.LEFT, padx=10, pady=10)
+        self.b3.pack(in_=self, side=tk.RIGHT, padx=10, pady=10)
         print(gv.recordedMovements)
 
     def cont(self):
@@ -39,3 +43,9 @@ class Menu(tk.Frame):
         for widget in self.winfo_children():
             widget.pack_forget()
         Mouvements(self)
+        
+    def train(self):
+        # supression des widgets présents sur la fenêtre
+        for widget in self.winfo_children():
+            widget.pack_forget()
+        MouvTraining(self)
